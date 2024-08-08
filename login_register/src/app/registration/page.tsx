185f2df65c49cb926b00b34eeffd67affec52cf3
CarlosEllerma <carlosm.ellerma@gmail.com>
2024-08-09 00:16:30 +0800

    follow up commit

--- login_register/src/app/registration/page.tsx
+++ login_register/src/app/registration/page.tsx
@@ -1,9 +1,80 @@
+import React from "react"
 
-export default function Registration() {
-    return (
-      <main>
-<h1>Hello</h1>
-      </main>
-    );
-  }
-  
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <main>
+      <div className="flex justify-center pt-16">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Register Account</CardTitle>
+            <CardTitle className="text-lg"> Enter Personal Details </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              {/* Name of the user (First, Middle, Last) */}
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="firstName">First Name:</Label>
+                  <Input type="text" id="firstName" placeholder="Enter your first name" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="middleName">Middle Name:</Label>
+                  <Input type="text" id="middleName" placeholder="Enter your middle name" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="lastName">Last Name:</Label>
+                  <Input type="text" id="lastName" placeholder="Enter your last name" />
+                </div>
+                {/* Sex of the user */}
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="sex">Sex:</Label>
+                  <Select>
+                    <SelectTrigger id="sex">
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="male">Male</SelectItem>
+                      <SelectItem value="female">Female</SelectItem>
+                      <SelectItem value="others">Others</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                {/* Birthday of the user */}
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="birthday">Birthday:</Label>
+                  <Input type="date" id="birthday" />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Link href="/registration1">
+                <Button type="submit">Next</Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+  );
+}
--- login_register/src/app/registration1/page.tsx
+++ login_register/src/app/registration1/page.tsx
@@ -11,7 +11,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import Link from "next/link";
 
 export default function email() {
   return (
@@ -24,23 +24,31 @@ export default function email() {
           </CardHeader>
           <CardContent>
             <form>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Username</Label>
+            <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email:</Label>
                 <Input id="Username" placeholder="Enter username" />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Password</Label>
+                <Label htmlFor="name">Username:</Label>
+                <Input id="Username" placeholder="Enter username" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Password:</Label>
                 <Input id="name" placeholder="Enter password" />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Confirm Password</Label>
+                <Label htmlFor="name">Confirm Password:</Label>
                 <Input id="password" placeholder="Confirm password" />
+              </div>
               </div>
             </form>
           </CardContent>
           <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Next</Button>
+            <Link href="/registration">
+            <Button variant="outline">Back</Button>
+            </Link>
+            <Button>Submit</Button>
           </CardFooter>
         </Card>
       </div>
